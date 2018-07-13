583c9f8323cbdd563813b37297b3c766ff48e96b
mgrunsfeld <41110853+mgrunsfeld@users.noreply.github.com>
2018-07-13 15:26:08 +0200

    Add files via upload

--- motorTests/motorTest.py
+++ motorTests/motorTest.py
@@ -4,13 +4,13 @@ stepPIN = 3  #orange
 dirPIN = 2 #yellow
 GPIO.setmode(GPIO.RAW)
 GPIO.setwarnings(False)
-GPIO.setup(3,GPIO.OUT,initial = 0)
+GPIO.setup(3,GPIO.OUT)
 GPIO.setup(2,GPIO.OUT, initial = 1)
 
-for i in range(0,200):
-	GPIO.output(stepPIN,1)
-	time.sleep(.001)
-	GPIO.output(stepPIN,0)
-	time.sleep(.1)
+step = GPIO.PWM(3,1)
+step.start(50)
+time.sleep(10)
+step.stop()
+	
 GPIO.cleanup()
 	
