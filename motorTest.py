91c7a9cc996a3eaa5b78eafa4ade80c81e643e48
mgrunsfeld <41110853+mgrunsfeld@users.noreply.github.com>
2018-07-13 15:03:07 +0200

    Add files via upload

---
+++ motorTest.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+stepPIN = 3  #orange
+dirPIN = 2 #yellow
+GPIO.setmode(GPIO.RAW)
+GPIO.setwarnings(False)
+GPIO.setup(3,GPIO.OUT,initial = 0)
+GPIO.setup(2,GPIO.OUT, initial = 1)
+
+for i in range(0,200):
+	GPIO.output(stepPIN,1)
+	time.sleep(.001)
+	GPIO.output(stepPIN,0)
+	time.sleep(.1)
+GPIO.cleanup()
+	
