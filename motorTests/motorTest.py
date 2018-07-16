e41eb6fcf8a67edccf5d4b70ca30070288e36888
mgrunsfeld <41110853+mgrunsfeld@users.noreply.github.com>
2018-07-16 13:25:22 +0200

    Add files via upload

--- motorTests/motorTest.py
+++ motorTests/motorTest.py
@@ -8,16 +8,16 @@ GPIO.setup(3,GPIO.OUT,initial = 0)
 GPIO.setup(2,GPIO.OUT, initial = 0)
 
 
-for i in range(0,100):
+for i in range(0,1000):
 	GPIO.output(stepPIN,1)
 	time.sleep(.000003)
 	GPIO.output(stepPIN,0)
-	time.sleep(.000003)
-for i in range(101,200):
+	time.sleep(.0003)
+for i in range(1001,2000):
 	GPIO.output(dirPIN, 1)
 	GPIO.output(stepPIN,1)
 	time.sleep(.000003)
 	GPIO.output(stepPIN,0)
-	time.sleep(.000003)
+	time.sleep(.0003)
 GPIO.cleanup()
 	
