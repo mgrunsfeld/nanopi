a985ad0e429d35520e56d87f9798656c4447e819
mgrunsfeld <41110853+mgrunsfeld@users.noreply.github.com>
2018-07-16 12:38:46 +0200

    Add files via upload

--- motorTests/motorTest.py
+++ motorTests/motorTest.py
@@ -5,13 +5,20 @@ dirPIN = 2 #yellow
 GPIO.setmode(GPIO.RAW)
 GPIO.setwarnings(False)
 GPIO.setup(3,GPIO.OUT,initial = 0)
-GPIO.setup(2,GPIO.OUT, initial = 1)
+GPIO.setup(2,GPIO.OUT, initial = 0)
 
 
 for i in range(0,200):
-	GPIO.output(stepPIN,1)
-	time.sleep(.000003)
-	GPIO.output(stepPIN,0)
-	time.sleep(.000003)
+	for i <= 100:
+		GPIO.output(stepPIN,1)
+		time.sleep(.000003)
+		GPIO.output(stepPIN,0)
+		time.sleep(.000003)
+	for i > 101:
+		GPIO.output(dirPIN, 1)
+		GPIO.output(stepPIN,1)
+		time.sleep(.000003)
+		GPIO.output(stepPIN,0)
+		time.sleep(.000003)
 GPIO.cleanup()
 	
--- motorTests/motorTestPWM.py
+++ motorTests/motorTestPWM.py
@@ -5,7 +5,7 @@ dirPIN = 2 #yellow
 GPIO.setmode(GPIO.RAW)
 GPIO.setwarnings(False)
 GPIO.setup(3,GPIO.OUT,initial = 0)
-GPIO.setup(2,GPIO.OUT, initial = 1)
+GPIO.setup(2,GPIO.OUT, initial = 0)
 freq = 250000
 dc = 50
 step = GPIO.PWM(3, freq)
