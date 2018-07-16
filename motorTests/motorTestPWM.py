73baa9a92c7f262b9985b24aef9ea093ec29adac
mgrunsfeld <41110853+mgrunsfeld@users.noreply.github.com>
2018-07-16 14:03:46 +0200

    Add files via upload

--- motorTests/motorTestPWM.py
+++ motorTests/motorTestPWM.py
@@ -5,8 +5,8 @@ dirPIN = 2 #yellow
 GPIO.setmode(GPIO.RAW)
 GPIO.setwarnings(False)
 GPIO.setup(3,GPIO.OUT,initial = 0)
-GPIO.setup(2,GPIO.OUT, initial = 0)
-freq = 250000
+GPIO.setup(2,GPIO.OUT, initial = 1)
+freq = 240000
 dc = 50
 step = GPIO.PWM(3, freq)
 step.start(dc)
