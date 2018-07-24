2ccd9dcaa604e9c7b3893e2524eb94e02fed8bc8
mgrunsfeld <41110853+mgrunsfeld@users.noreply.github.com>
2018-07-24 15:13:33 +0200

    Update RF_Test.py

--- RF_Test/RF_Test.py
+++ RF_Test/RF_Test.py
@@ -6,5 +6,5 @@ ser = serial.Serial('/dev/ttyS1',57600,timeout = 2)
 while True:
 	name = ser.read(2)
 	if str(name) == 'A1':
-		ser.write('\r\n You called?')
+		ser.write('\r\n You called?\r\n')
 		continue
